ee289c147f9ba19f44b714060770a98b5eadc921
shailesh-00git <sy468698@gmail.com>
2026-04-16 19:07:46 +0545

    added the home page ui and post fetch logic

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,16 @@
 import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import {
+  Empty,
+  EmptyContent,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle,
+} from "@/components/ui/empty";
 import { createClient } from "@/lib/supabase/server";
+import { ArrowUpRightIcon, Folder } from "lucide-react";
+import Link from "next/link";
 
 export default async function Home() {
   const supabase = await createClient();
@@ -7,20 +18,73 @@ export default async function Home() {
     data: { user },
   } = await supabase.auth.getUser();
 
-  return (
-    <div className="min-h-screen w-full">
-      <nav className="fixed top-4 left-1/2 -translate-x-1/2 z-50 w-full max-w-5xl px-4 py-3 rounded-xl bg-zinc-300 backdrop-blur-md border shadow-lg shadow-zinc-400">
-        Navbar
-      </nav>
+  // posts
+  const { data: posts } = await supabase
+    .from("posts")
+    .select("*")
+    .eq("user_id", user.id)
+    .order("created_at", { ascending: false });
+  console.log(posts);
 
-      <div className="min-h-screen grid place-content-center max-w-7xl bg-zinc-200 mx-auto">
-        <form action="/auth/logout" method="POST">
-          {JSON.stringify(user)}
-          <Button className="p-5" type="submit">
-            Logout
-          </Button>
-        </form>
+  return (
+    <main className="min-h-screen w-full bg-zinc-800">
+      <div className="max-w-5xl mx-auto px-8 py-6">
+        {/* navbar */}
+        <div className="flex  justify-between items-center mb-8">
+          <h1 className="text-4xl font-semibold text-emerald-400">My Blogs</h1>
+          <div className="flex gap-2 items-center">
+            <Link href={"/editor"}>
+              <Button className="p-5 bg-emerald-400 text-black hover:bg-black hover:text-emerald-400 hover:border-emerald-400 transition-colors duration-150">
+                New Post
+              </Button>
+            </Link>
+            <form action="/auth/logout" method="POST">
+              <Button
+                className="p-5  border border-emerald-400 text-emerald-400 hover:text-red-500 hover:border-red-500 "
+                type="submit"
+              >
+                Logout
+              </Button>
+            </form>
+          </div>
+        </div>
+
+        {/* herosection */}
+        {posts && posts.length > 0 ? (
+          <div className="grid gap-4 text-zinc-300">
+            {posts.map((post) => (
+              <Card className={"bg-zinc-700 p-4"} key={post.id}>
+                <h2>{post.title}</h2>
+                {post.excerpt && <div>{post.excerpt}</div>}
+                {post.image && <div className="h-20 w-15">{post.image}</div>}
+                <p>{new Date(post.created_at).toLocaleDateString()}</p>
+              </Card>
+            ))}
+          </div>
+        ) : (
+          <Empty className="border border-zinc-400 p-12">
+            <EmptyHeader>
+              <EmptyMedia variant="icon">
+                <Folder></Folder>
+              </EmptyMedia>
+              <EmptyTitle className={"text-2xl text-zinc-400"}>
+                No Blogs Yet
+              </EmptyTitle>
+              <EmptyDescription>
+                You haven&apos;t created any blogs yet. Get started by creating
+                your first blog.
+              </EmptyDescription>
+            </EmptyHeader>
+            <EmptyContent className="flex-row justify-center gap-2">
+              <Link href={"/editor"}>
+                <Button className="p-5 bg-emerald-400 text-black hover:bg-black hover:text-emerald-400 hover:border-emerald-400 transition-colors duration-150">
+                  create blog
+                </Button>
+              </Link>
+            </EmptyContent>
+          </Empty>
+        )}
       </div>
-    </div>
+    </main>
   );
 }
